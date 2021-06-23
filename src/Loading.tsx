62e2b8a84d55b586aba0c6b4bfc7b476c52f2421
geneprophet <geneprophet@163.com>
2021-06-23 20:06:21 +0800

    loading

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import { LoadingOutlined } from '@ant-design/icons';
 const Loading = () => {
-  return <div>loading....</div>;
+  return (
+    <div>
+      <LoadingOutlined />
+      loading....
+    </div>
+  );
 };
 
 export default Loading;
